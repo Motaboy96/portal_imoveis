efd3f661b8757c1fae184241d11215546bd18dbd
Motaboy96 <luanmota2007@hotmail.com>
2025-03-19 18:15:47 -0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portal_imoveisdb',
+        'USER': 'admin',
+        'PASSWORD': 'Gf4Y741zxBjhRUmLmLIKF340xM1RdqN2',
+        'HOST': 'dpg-cvdigoij1k6c73ce0ot0-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
 
