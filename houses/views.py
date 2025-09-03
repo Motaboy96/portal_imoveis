a2230ad875c318cd7859b015b6a6117ef0345b9c
Motaboy96 <luanmota2007@hotmail.com>
2025-09-02 22:27:19 -0300

    Update views.py

--- houses/views.py
+++ houses/views.py
@@ -13,7 +13,7 @@ class ImoveisListView(ListView):
     context_object_name = 'imoveis'
 
     def get_queryset(self):
-        # queryset = Imoveis.objects.all().order_by('bairro')
+     
         queryset = super().get_queryset().order_by('bairro')
 
         search = self.request.GET.get('search')
@@ -42,26 +42,26 @@ class ImoveisRentListView(ListView):
     context_object_name = 'imoveis_rent'
 
     def get_queryset(self):
-        # Filtra imóveis com 'pretensao' igual a 'Aluguel'
+        
         queryset = Imoveis.objects.filter(pretensao='Aluguel')
 
-        # Captura os parâmetros de filtro
+        
         tipo = self.request.GET.get('tipo')
         search = self.request.GET.get('search')
 
-        # Aplica o filtro de 'search' (bairro, cidade ou condominio)
+        
         if search:
             queryset = queryset.filter(
-                Q(bairro__bairro__icontains=search) |  # Buscar no bairro
-                Q(cidade__icontains=search) |         # Buscar na cidade
-                Q(condominio__icontains=search)       # Buscar no condomínio (se esse campo existir no modelo)
+                Q(bairro__bairro__icontains=search) |  
+                Q(cidade__icontains=search) |         
+                Q(condominio__icontains=search)       
             )
 
-        # Aplica o filtro de 'tipo' (Apartamento, Casa, etc.)
+        
         if tipo:
             queryset = queryset.filter(tipo=tipo)
 
-        # Retorna o queryset filtrado
+        
         return queryset
 
 
